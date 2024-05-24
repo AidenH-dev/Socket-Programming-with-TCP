389bcbc8862c07ab0095bf47982ef2dc391ced4e
Aiden <aiden.habboub@gmail.com>
2024-05-24 10:38:24 -0400

    added files

--- server.py
+++ server.py
@@ -2,6 +2,8 @@
 import socket
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# now connect to the web server on port 80 - the normal http port
-s.connect(("www.python.org", 80))
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# bind the socket to a public host, and a well-known port
+serversocket.bind((socket.gethostname(), 80))
+# become a server socket
+serversocket.listen(5)
